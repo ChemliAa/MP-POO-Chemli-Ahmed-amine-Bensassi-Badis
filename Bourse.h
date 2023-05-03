2ae12828ae172e1ed7b0190ee8452391f0dfd923
ChemliAa <ahmadaminechemli@gmail.com>
2023-05-03 17:04:32 +0100

    changed some stuff v2

--- Bourse.h
+++ Bourse.h
@@ -11,11 +11,13 @@ class Bourse{
     Bourse(const Date& date):dateCourante(date){}
     virtual ~Bourse(){};
     Date getDateFinRech()const{return dateCourante;}
-    virtual vector<string> getActionsDisponiblesParDate(const Date&)=0;
+    virtual vector<string> getActionsDisponiblesParDate(const Date&)const=0;
     virtual vector<PrixJournalier> getPrixJournaliersParDate(const Date&)const =0;
     virtual vector<PrixJournalier> getPrixJournaliersParDateEtPrix(const Date& date,double soldeCourant)const =0;
     virtual double getPrixParDateEtAction(const Date& date,string action)const =0;
     virtual double dernierPrixDuneAction(const Date& date,string action)const =0;
     virtual void setDateCourante(Date date)=0;
+    virtual bool isActionDisponibleParDate(const Date& date,string action)const=0;
+
 };
 #endif // BOURSE_H
