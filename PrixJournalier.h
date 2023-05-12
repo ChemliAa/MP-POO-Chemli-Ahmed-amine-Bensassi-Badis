a41635ccf4e84c67c04bb68dc01b02523932a6cf
ChemliAa <ahmadaminechemli@gmail.com>
2023-05-12 17:41:35 +0100

    Added constructor with nomAction Parameters left previous constructors intact

--- PrixJournalier.h
+++ PrixJournalier.h
@@ -10,7 +10,7 @@ private:
 public:
     friend istream& operator>> (istream& flux, PrixJournalier& pj );
     friend bool operator<(const PrixJournalier& pj1,const PrixJournalier& pj2);
-    PrixJournalier(Date datePrixJournalier,double prix=0):date(datePrixJournalier),nomAction(""),prix(prix){};
+    PrixJournalier(Date datePrixJournalier,string nomAction="",double prix=0):date(datePrixJournalier),nomAction(nomAction),prix(prix){};
     PrixJournalier(int d=1,int m=1,int y=2000):date(d,m,y),nomAction(""),prix(0){};
     Date getDate()const;
     string getNomAction()const;
